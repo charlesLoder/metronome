b6c11cd48abb17003ad16a896541d6d69a4b4199
Charles Loder <charles.loder@rutgers.edu>
2017-12-15 10:26:39 -0500

    beep at speed set by user input

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-const playBeep = function () {
+var totalBeeps = 1; // everything related to totalBeeps will eventually be changed
+
+const playBeep = function (bpm) {
+  bpm = choiceBpm();
+  speed = setTimeout(playBeep, (60000 / bpm));
   document.getElementById("accent").play();
+
+  totalBeeps++;
+  if (totalBeeps > 8) {
+    clearTimeout(speed);
+  }
+  
+}
+
+const choiceBpm = function (input) {
+  input = document.getElementById("userBpm").value;
+  return input;
 }
