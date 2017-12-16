025b9f36fd52ec6e1a1f1a8da8aea6f272fa09e1
Charles Loder <charles.loder@rutgers.edu>
2017-12-16 16:29:58 -0500

    playBeep takes choiceBpm as bpm parameter

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 var totalBeeps = 1; // everything related to totalBeeps will eventually be changed
 
 const playBeep = function (bpm) {
-  bpm = choiceBpm();
-  speed = setTimeout(playBeep, (60000 / bpm));
+  tempo = setTimeout( function() {playBeep(bpm)} , (60000 / bpm));
   document.getElementById("accent").play();
   console.log("beep");
-
+  //
   totalBeeps++;
   if (totalBeeps > 8) {
-    clearTimeout(speed);
+    clearTimeout(tempo);
   }
 
 }
 
-const choiceBpm = function (input) {
+const choiceBpm = function () {
   input = document.getElementById("userBpm").value;
-  return input;
+  return eval(input);
 }
 
 document.getElementById("start").addEventListener("click", () => {
-  playBeep();
+  playBeep(choiceBpm());
 })
