dad6cc46fd27f4e5cf37dd75bab8a309339bbc7f
Charles Loder <charles.loder@rutgers.edu>
2017-12-19 09:22:07 -0500

    bpb determined by length of ul not BPB input

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const choiceBpm = function (input) {
   return eval(input);
 }
 
-const choiceBpb = function (input) {
-  return eval(input);
-}
+document.getElementById("addBpb").addEventListener("click", () => {
+  let beatList = document.getElementById("beatList");
+  let beat = document.createElement("li");
+  let beatButton = document.createElement("button");
+  beatButton.appendChild(document.createTextNode( document.getElementById("beatList").getElementsByTagName("li").length + 1) );
+  beat.appendChild(beatButton);
+  beatList.appendChild(beat);
+})
+
 
 const playBeep = function (bpm, bpb, currentBeat) {
   let tempo = setTimeout( function() {playBeep(bpm, bpb, currentBeat)} , (60000 / bpm));
@@ -19,7 +25,7 @@ const playBeep = function (bpm, bpb, currentBeat) {
     currentBeat = 1;
     // console.log("beep");
     document.getElementById("noAccent").play();
-    clearTimeout(tempo); // this is just to stop it after a while
+    // clearTimeout(tempo); // this is just to stop it after a while
   } else {
     // console.log("beep");
     document.getElementById("noAccent").play();
@@ -30,6 +36,9 @@ const playBeep = function (bpm, bpb, currentBeat) {
 
 document.getElementById("start").addEventListener("click", () => {
   let userBpmInput = document.getElementById("userBpm").value;
-  let userBpbInput = document.getElementById("userBpb").value;
-  playBeep(choiceBpm(userBpmInput), choiceBpb(userBpbInput), 1);
+  let userBpbInput = document.getElementById("beatList").getElementsByTagName("li").length;
+  playBeep(choiceBpm(userBpmInput), userBpbInput, 1);
 })
+
+// maybe, add an ul of buttons
+// that would bpb = length of the ul (i.e. document.getElementsByTagName("ul").length)
