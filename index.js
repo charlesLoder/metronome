afa44a08ada2e55a5f37536f73e64489c8926ed8
Charles Loder <charles.loder@rutgers.edu>
2017-12-19 08:40:46 -0500

    Will play accent on beat 1, or no accent on others

--- index.js
+++ index.js
@@ -8,16 +8,24 @@ const choiceBpb = function (input) {
 
 const playBeep = function (bpm, bpb, currentBeat) {
   let tempo = setTimeout( function() {playBeep(bpm, bpb, currentBeat)} , (60000 / bpm));
-  // document.getElementById("accent").play();
-  console.log("beep");
-  console.log(currentBeat);
-  //
-  currentBeat++;
-  if (currentBeat > bpb) {
+
+  if (currentBeat == 1) {
+    // console.log("beep!");
+    document.getElementById("accent").play();
+    console.log(currentBeat);
+    currentBeat++;
+  } else if (currentBeat >= bpb) {
+    console.log(currentBeat);
     currentBeat = 1;
-    clearTimeout(tempo);
+    // console.log("beep");
+    document.getElementById("noAccent").play();
+    clearTimeout(tempo); // this is just to stop it after a while
+  } else {
+    // console.log("beep");
+    document.getElementById("noAccent").play();
+    console.log(currentBeat);
+    currentBeat++;
   }
-  //
 }
 
 document.getElementById("start").addEventListener("click", () => {
